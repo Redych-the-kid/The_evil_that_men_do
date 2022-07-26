24db5fd2ed1828fa5020900c89bea813f2fde307
Redych_the_kid <79579497+Redych-the-kid@users.noreply.github.com>
2022-07-26 20:22:29 +0700

    Updated the connection

--- Lab3/src/main/java/Connection.java
+++ Lab3/src/main/java/Connection.java
@@ -1,22 +1,23 @@
 import java.io.IOException;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 
 /*
-	Connection class to make a connection with a peer.We give port, name and ip and connection list
+	Connection class to make a connection with a peer.We give port, name and ip and connection list,
 	and it waits 15 seconds to be sure that peer is online and then creates socket and puts it in the connections list.
  */
 public class Connection implements Runnable
 {
-	private final ConcurrentHashMap<String, Socket> connections; // Connections
+	private final ConcurrentHashMap<String, SocketChannel> connections; // Connections
 	private final String name; // Peer name
 	private final String host_ip; // Peer host ip
 	private final int port; // Peer
 
 	public Connection(int port, String name, String host_ip,
-					  ConcurrentHashMap<String, Socket> connections)
+					  ConcurrentHashMap<String, SocketChannel> connections)
 	{
 		this.connections = connections;
 		this.port = port;
@@ -30,7 +31,7 @@ public class Connection implements Runnable
 		try
 		{
 			Thread.sleep(15000); // ~15 seconds
-			Socket socket = new Socket(host_ip, port);
+			SocketChannel socket = SocketChannel.open(new InetSocketAddress(host_ip, port));
 			connections.put(name, socket);
 		}
 		catch(IOException | InterruptedException e)
